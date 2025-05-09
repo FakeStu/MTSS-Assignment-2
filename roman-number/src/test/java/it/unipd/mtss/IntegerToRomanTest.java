779df04bee1998dc2f1146f56a8026bdda5ccca1
tuoni <tuoniangelo@gmail.com>
2025-05-09 17:12:56 +0200

    Test10
    close #7

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -13,9 +13,12 @@ public class IntegerToRomanTest {
         assertEquals("I", IntegerToRoman.convert(1));
         assertEquals("II", IntegerToRoman.convert(2));
         assertEquals("III", IntegerToRoman.convert(3));
- 	assertEquals("IV", IntegerToRoman.convert(4));
+ 	    assertEquals("IV", IntegerToRoman.convert(4));
         assertEquals("V", IntegerToRoman.convert(5));
         assertEquals("VI", IntegerToRoman.convert(6));
-
+        assertEquals("VII", IntegerToRoman.convert(7));
+        assertEquals("VIII", IntegerToRoman.convert(8));
+        assertEquals("IX", IntegerToRoman.convert(9));
+        assertEquals("X", IntegerToRoman.convert(10));
     }
 }
