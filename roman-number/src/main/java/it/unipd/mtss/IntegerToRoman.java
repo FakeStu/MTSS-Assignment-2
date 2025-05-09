e9fe3fcd21091f0f85921ac4fc2a35b50097670d
oSnoopyy <mattia.olivamedin@studenti.unipd.it>
2025-05-09 16:24:01 +0200

    IntegerToRoman
    close #2

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -1,2 +1,24 @@
 // Angelo Tuoni         2116405
 // Mattia Oliva Medin   2103471
+
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+    public static String convert(int number) {
+         Definizione delle lettere romane e dei loro valori corrispondenti
+        String[] romanLetters = {I};
+        int[] values = {1};
+
+        StringBuilder romanNumber = new StringBuilder();
+
+         Itera attraverso i valori romani e aggiungi le lettere corrispondenti
+        for (int i = 0; i  values.length; i++) {
+            while (number = values[i]) {
+                number -= values[i];
+                romanNumber.append(romanLetters[i]);
+            }
+        }
+
+        return romanNumber.toString();
+    }
+}
